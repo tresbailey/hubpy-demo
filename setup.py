ca479b1a255e3d39ada6945f8307c6ad9aff2049
tresbailey <tres.bailey@gmail.com>
2013-09-30 23:05:13 -0400

    Resetting setup and application py files for flask

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Todo Demo',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='Tres\' Bailey',
+      author_email='tres.bailey@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=[ 'Flask==0.8', 'MongoAlchemy==0.11',
+        'Werkzeug==0.8.3',
+        'pymongo==2.1.1', 'python-dateutil==1.5',
+        'redis==2.4.11', 'simplejson==2.1.6', 
+        'wsgiref==0.1.2',
+        'Flask-MongoAlchemy==0.5.3', 'flask_oauth==0.12']
      )
